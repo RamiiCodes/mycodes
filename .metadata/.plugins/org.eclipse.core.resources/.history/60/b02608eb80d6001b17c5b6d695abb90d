7b6150028b48f8eefaf63ef0dcf28d679874579a
rami <raami.ammar@gmail.com>
2021-06-26 15:18:32 +0200

    add code in class multiple of 3 and 5

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/60/b02608eb80d6001b17c5b6d695abb90d
@@ -0,0 +1,10 @@
+package mycodes;
+
+public class CDMultipleof3and5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- mycodes/src/mycodes/CDMultipleof3and5.java
+++ mycodes/src/mycodes/CDMultipleof3and5.java
@@ -1,10 +1,16 @@
 package mycodes;
 
+import java.util.stream.IntStream;
+
 public class CDMultipleof3and5 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public int solution (int number) {
+		return IntStream.range(1, number).filter(n -> n%3 ==0 || n % 5 ==0).sum();
+	  }
+/*IntStream range(int startInclusive, int endExclusive) returns a sequential ordered IntStream 
+ * from startInclusive (inclusive) to endExclusive (exclusive) by an incremental step of 1.
+ * *****************************************************************************************
+ * The filter() method is an intermediate operation of the Stream interface that allows us
+ *  to filter elements of a stream that match a given Predicate
+ *  in java 8 , we can add lambda expression into filter parameters */
 
 }
